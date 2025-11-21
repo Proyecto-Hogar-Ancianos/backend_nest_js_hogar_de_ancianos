a39a39a25b83a7ea959369013d4761fcfd195adb
ANTONY.MONGELOPEZ <antony.mongelopez@ucr.ac.cr>
2025-11-20 19:55:36 -0600

    improve: configurar jest para generar reportes en XML

--- jenkins/tony/scripts/test-runner.groovy
+++ jenkins/tony/scripts/test-runner.groovy
@@ -3,11 +3,11 @@ def installDependencies() {
 }
 
 def runJestTests() {
-    bat 'npm run test -- --coverage --testPathPatterns="tests/tony/unit" > jest-results.log 2>&1'
+    bat 'npm run test -- --coverage --testPathPatterns="tests/tony/unit" --reporters=default --reporters=jest-junit > jest-results.log 2>&1'
 }
 
 def runUnitTests() {
-    bat 'npm run test -- tests/tony/unit/users/user.service.spec.ts --verbose > unit-test-results.log 2>&1'
+    bat 'npm run test -- tests/tony/unit/users/user.service.spec.ts --verbose --reporters=default --reporters=jest-junit > unit-test-results.log 2>&1'
 }
 
 def getTestOutput(logFile, maxLines = 500) {
