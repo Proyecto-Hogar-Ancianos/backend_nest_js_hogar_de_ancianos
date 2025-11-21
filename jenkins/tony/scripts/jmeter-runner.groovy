33bc9d418ff11128beb0e89e8d7bfec7336a5cfe
ANTONY.MONGELOPEZ <antony.mongelopez@ucr.ac.cr>
2025-11-20 20:57:51 -0600

    refactor: mover todos los archivos de jmeter a jenkins/tony/ para mantener aislamiento

--- jenkins/tony/scripts/jmeter-runner.groovy
+++ jenkins/tony/scripts/jmeter-runner.groovy
@@ -3,7 +3,7 @@ def runJMeterTests() {
         @echo off
         setlocal enabledelayedexpansion
         
-        if not exist "jmeter-results" mkdir jmeter-results
+        if not exist "jenkins\\tony\\jmeter-results" mkdir jenkins\\tony\\jmeter-results
         echo [JMeter] Directorio jmeter-results creado/verificado
         
         where jmeter >nul 2>&1
@@ -15,38 +15,38 @@ def runJMeterTests() {
                 echo ^<sample t="150" it="0" ct="0" by="2048" rc="200" rm="OK" s="true" lb="GET /api/users" hn="localhost" tn="Users Thread Group" tt="1000" dt="html" de="UTF-8" by="2048" sc="1" ec="0" hn="localhost" /^>
                 echo ^<sample t="200" it="0" ct="0" by="3072" rc="200" rm="OK" s="true" lb="POST /api/users" hn="localhost" tn="Users Thread Group" tt="1000" dt="html" de="UTF-8" by="3072" sc="1" ec="0" hn="localhost" /^>
                 echo ^</testResults^>
-            ) > jmeter-results/results.jtl
-            echo [JMeter] Resultados dummy creados en jmeter-results/results.jtl
+            ) > jenkins\\tony\\jmeter-results\\results.jtl
+            echo [JMeter] Resultados dummy creados en jenkins/tony/jmeter-results/results.jtl
             exit /b 0
         )
         
         echo [JMeter] JMeter encontrado en PATH
         
-        if exist "tests/jmeter/users-performance-test.jmx" (
+        if exist "tests\\tony\\jmeter\\users-performance-test.jmx" (
             echo [JMeter] Archivo de prueba encontrado. Ejecutando JMeter...
-            jmeter -n -t tests/jmeter/users-performance-test.jmx -l jmeter-results/results.jtl -j jmeter.log
-            if exist "jmeter-results/results.jtl" (
+            jmeter -n -t tests\\tony\\jmeter\\users-performance-test.jmx -l jenkins\\tony\\jmeter-results\\results.jtl -j jenkins\\tony\\jmeter.log
+            if exist "jenkins\\tony\\jmeter-results\\results.jtl" (
                 echo [JMeter] Archivo de resultados creado exitosamente
-                type jmeter-results/results.jtl
+                type jenkins\\tony\\jmeter-results\\results.jtl
             ) else (
                 echo [JMeter] ERROR: No se creo el archivo de resultados
             )
         ) else (
-            echo [JMeter] ERROR: Archivo de prueba no encontrado en tests/jmeter/users-performance-test.jmx
+            echo [JMeter] ERROR: Archivo de prueba no encontrado en tests/tony/jmeter/users-performance-test.jmx
             exit /b 0
         )
     '''
 }
 
 def getJMeterResults() {
-    if (fileExists('jmeter-results/results.jtl')) {
-        def content = readFile('jmeter-results/results.jtl')
+    if (fileExists('jenkins/tony/jmeter-results/results.jtl')) {
+        def content = readFile('jenkins/tony/jmeter-results/results.jtl')
         if (content && content.length() > 0) {
             return content.take(1500)
         }
         return 'Archivo de resultados JMeter vacio'
     }
-    return 'ERROR: No se encontro el archivo jmeter-results/results.jtl'
+    return 'ERROR: No se encontro el archivo jenkins/tony/jmeter-results/results.jtl'
 }
 
 return this
