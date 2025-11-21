fef431226458fa413272fe2c7644471c070e5858
LUIS.RIVERALOPEZ <luis.riveralopez@ucr.ac.cr>
2025-11-20 22:48:23 -0600

    m

--- tests/luis/unit/entrance-exit/entrance-exit.service.spec.ts
+++ tests/luis/unit/entrance-exit/entrance-exit.service.spec.ts
@@ -160,7 +160,7 @@ describe('create - Prueba 2', () => {
   });
 
   describe('create - Prueba 5', () => {
-    it('debería usar la fecha actual cuando no se proporciona fecha de entrada', async () => {
+    it('debería usar la fecha actual cuando no se proporciona fecha de entradaaa', async () => {
       const createDto: CreateEntranceExitDto = {
         eeType: EntranceExitType.VISITOR,
         eeAccessType: AccessType.ENTRANCE,
